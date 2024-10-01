a4de43be5e66ac0c03d83f081319b300623543a5
Ashmin <ashmin2802@gmail.com>
2024-10-01 13:34:20 +0530

    db

--- connection/db.js
+++ connection/db.js
@@ -1,9 +1,17 @@
-const mongoose=require('mongoose')
+const mongoose = require('mongoose');
 
-const connectionString=process.env.DATABASE
+// Get the MongoDB connection string from environment variables
+const connectionString = process.env.DATABASE;
 
-mongoose.connect(connectionString).then((res)=>{
-    console.log("Home Successfully connected with Atles..!");
-}).catch((err)=>{
-    console.log(err);
+// Configure the Mongoose connection
+mongoose.connect(connectionString, {
+    useNewUrlParser: true,        // Use new URL string parser
+    useUnifiedTopology: true,     // Use the new unified topology engine
+    serverSelectionTimeoutMS: 10000,  // Set a timeout for initial server selection
 })
+.then(() => {
+    console.log("Successfully connected to MongoDB Atlas!");
+})
+.catch((err) => {
+    console.error("Connection error:", err);
+});
